d4090acf2960e202f2823ca836f1867da79adee1
Anton <didenkofficial@gmail.com>
2022-04-24 23:11:16 +0300

    Оптимізував 1 задачу

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,26 @@
 const itemsNumberEl = document.querySelectorAll('.item');
 console.log('Number of categories:', itemsNumberEl.length);
 
-const headerEl = document.querySelectorAll('li > h2');
-const listChildrenEl = document.querySelectorAll('.item__list');
+// ==================1 варіант===========================
+// const headerEl = document.querySelectorAll('li > h2');
+// const listChildrenEl = document.querySelectorAll('.item__list');
 
-const firstHeaderEl = headerEl[0].textContent;
-console.log('Category:', firstHeaderEl);
-console.log('Elements:', listChildrenEl[0].children.length);
+// const firstHeaderEl = headerEl[0].textContent;
+// console.log('Category:', firstHeaderEl);
+// console.log('Elements:', listChildrenEl[0].children.length);
 
-const secondHeaderEl = headerEl[1].textContent;
-console.log('Category:', secondHeaderEl);
-console.log('Elements:', listChildrenEl[1].children.length);
+// const secondHeaderEl = headerEl[1].textContent;
+// console.log('Category:', secondHeaderEl);
+// console.log('Elements:', listChildrenEl[1].children.length);
 
-const lastHeaderEl = headerEl[2].textContent;
-console.log('Category:', lastHeaderEl);
-console.log('Elements:', listChildrenEl[2].children.length);
+// const lastHeaderEl = headerEl[2].textContent;
+// console.log('Category:', lastHeaderEl);
+// console.log('Elements:', listChildrenEl[2].children.length);
+
+// ==================2 варіант===========================
+const ul = document.querySelectorAll('#categories > li')
+
+ul.forEach(el => {
+    console.log("Категорія:", el.firstElementChild.textContent);
+    console.log("Кількість елементів:", el.lastElementChild.children.length);
+});
