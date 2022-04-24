6b2dbeec3759d1f1716fa4c0de3672fedfd11b81
Anton <didenkofficial@gmail.com>
2022-04-24 23:16:52 +0300

    Видалив зайве з 10 задачі

--- js/task-10.js
+++ js/task-10.js
@@ -1,24 +1,3 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-
-// const inputEl = document.querySelector('#controls > input');
-// const btnCreate = document.querySelector('button[data-create]');
-// const btnDestroy = document.querySelector('button[data-destroy]');
-// const resultOfAction = document.querySelector('#boxes');
-
-// inputEl.addEventListener('input', (value) => {
-//   console.log(value.currentTarget.value);
-//   btnCreate.addEventListener('click', () => {
-//     const newEl = resultOfAction.createElement('div');
-    
-//   })
-// })
-
-// function createBoxes(amount) {
-  
-// }
-
 const render = document.querySelector('button[data-create]');
 const destroy = document.querySelector('button[data-destroy]');
 const boxes = document.getElementById("boxes");
